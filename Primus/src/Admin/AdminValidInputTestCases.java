d7a5398f8b2ee62cef54d882188ab1a714018401
yarra <yarra@LAPTOP-PT2GBA54>
2023-09-23 13:50:25 +0530

    TestCase AdminLogin

---
+++ Primus/src/Admin/AdminValidInputTestCases.java
@@ -0,0 +1,12 @@
+package Admin;
+
+public class AdminValidInputTestCases {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("This is Admin Login");
+	
+	}
+
+}
